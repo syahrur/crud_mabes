c27fd7ae8b9a9fd447d7793d19921d1282b588f4
syahrur <syahru6842.@gmail.com>
2019-04-16 08:40:52 +0700

    header

---
+++ application/views/header/header.php
@@ -0,0 +1,57 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Dashboard One | Notika - Notika Admin Template</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- favicon
+		============================================ -->
+    <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url().'assets/dashboard/img/favicon.ico'?>">
+    <!-- Google Fonts
+		============================================ -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
+    <!-- Bootstrap CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/dashboard/css/bootstrap.min.css'?>">
+    <!-- Bootstrap CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/dashboard/css/font-awesome.min.css'?>">
+    <!-- owl.carousel CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/dashboard/css/owl.carousel.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/dashboard/css/owl.theme.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/dashboard/css/owl.transitions.css'?>">
+    <!-- meanmenu CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/dashboard/css/meanmenu/meanmenu.min.css'?>">
+    <!-- animate CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/dashboard/css/animate.css'?>">
+    <!-- normalize CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/dashboard/css/normalize.css'?>">
+    <!-- mCustomScrollbar CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/dashboard/css/scrollbar/jquery.mCustomScrollbar.min.css'?>">
+    <!-- jvectormap CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/dashboard/css/jvectormap/jquery-jvectormap-2.0.3.css'?>">
+    <!-- notika icon CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/dashboard/css/notika-custom-icon.css'?>">
+    <!-- wave CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/dashboard/css/wave/waves.min.css'?>">
+    <!-- main CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/dashboard/css/main.css'?>">
+    <!-- style CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/dashboard/style.css'?>">
+    <!-- responsive CSS
+		============================================ -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/dashboard/css/responsive.css'?>">
+    <!-- modernizr JS
+		============================================ -->
+    <script src="<?php echo base_url().'assets/dashboard/js/vendor/modernizr-2.8.3.min.js'?>"></script>
+</head>
